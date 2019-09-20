bdabce5310b122b3ec7cd87a2c0c1b6e9f27578c
ahren <ahrenswett@gmail.com>
2019-09-20 10:53:15 -0700

    Created bitmap file ln

--- src/main/java/bitmap/transformer/bitmap.java
+++ src/main/java/bitmap/transformer/bitmap.java
@@ -1,8 +1,17 @@
 package bitmap.transformer;
+import java.io.File;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
 
 public class bitmap {
     public static void main(String[] args){
     }
+
+    File bitMap = new File("../../../../assets/mario.bmp");
+
+
 
     
 
