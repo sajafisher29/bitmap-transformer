c03778fdaedd8924bd4407f84cacc2e74326a6af
Sarah Fisher <sajafisher@gmail.com>
2019-09-20 13:43:08 -0700

    working on building image manipulation code

--- src/main/java/bitmap/transformer/Bitmap.java
+++ src/main/java/bitmap/transformer/Bitmap.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.nio.Buffer;
 import javax.imageio.ImageIO;
 
-
+//Reference material: https://www.youtube.com/watch?v=lGX0Gc6d51s&feature=youtu.be
 
 public class Bitmap {
     public static void main(String[] args)throws IOException {
@@ -26,6 +26,19 @@ public class Bitmap {
             System.out.println("Error: " + e);
         }
         System.out.println("Victory!");
+    }
+
+    //Reference material: https://www.dyclassroom.com/image-processing-project/how-to-get-and-set-pixel-value-in-java
+    public static void pixelManipulator(String colorWanted, String colorToChange, String imagePath) {
+        File path = new File(imagePath);
+        try {
+            BufferedImage img = ImageIO.read(path);
+        } catch (IOException e) {
+            System.out.println(imagePath + "Does not exist");
+            e.printStackTrace();
+        }
+        int p = image.getRGB();
+
     }
 }
 
